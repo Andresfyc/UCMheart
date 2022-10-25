4c5b87e51df42cb2e7cc443660feb008f5079456
Andres Yunda <62907237+Andresfyc@users.noreply.github.com>
2022-10-25 13:06:05 +0200

    Initial Commit

---
+++ UCMheart/UCMheartApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCMheartApp.swift
+//  UCMheart
+//
+//  Created by Andres Felipe Yunda Castillo on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UCMheartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
